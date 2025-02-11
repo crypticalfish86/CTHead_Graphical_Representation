62eac90cdc47caeafe0873c671f62a5c3f32ee37
crypticalfish86 <jasonweerawardena@gmail.com>
2025-02-11 12:16:37 +0000

    Simplified image width height and depth values to a constant value (equivalent dimensions)

--- src/Main.java
+++ src/Main.java
@@ -36,9 +36,7 @@ public class Main extends Application {
     float[][] maximumIntensityProjectionImageSide; //The MIP for the side view
     VBox sideVboxMIP;
 
-    public final static int IMAGE_WIDTH = 256; //the width of view in the dataset
-    public final static int IMAGE_HEIGHT = 256; //the height of every view in the dataset
-    public final static int IMAGE_DEPTH = 256; //the depth of every view in the dataset
+    public static final int DATA_SIZE = 256; //the constant width, height and depth of the dataset;
 
     short min = Short.MAX_VALUE; //minimum byte value in dataset (used in normalisation, initialised at max value)
     short max = Short.MIN_VALUE; //maximum byte value in dataset (used in normalisation, initialised at min value)
@@ -50,11 +48,11 @@ public class Main extends Application {
         File file = new File(CT_HEAD_FILEPATH);
         DataInputStream input = new DataInputStream(new BufferedInputStream(new FileInputStream(file)));
 
-        datasetVoxelByteValuesTop = new short[IMAGE_DEPTH][IMAGE_HEIGHT][IMAGE_WIDTH]; //initialize 3D array for 3D dataset
+        datasetVoxelByteValuesTop = new short[DATA_SIZE][DATA_SIZE][DATA_SIZE]; //initialize 3D array for 3D dataset
 
-        for (int slide = 0; slide < IMAGE_DEPTH; slide++) {
-            for (int row = 0; row < IMAGE_HEIGHT; row++) {
-                for (int col = 0; col < IMAGE_WIDTH; col++) {
+        for (int slide = 0; slide < DATA_SIZE; slide++) {
+            for (int row = 0; row < DATA_SIZE; row++) {
+                for (int col = 0; col < DATA_SIZE; col++) {
                     int firstByte = ((int) input.readByte()) & 0xff; //read in first byte of voxel
                     int secondByte = ((int) input.readByte()) & 0xff; //read in second byte of voxel
                     short read = (short) ((secondByte << 8) | firstByte); //"Swizzle" the bytes (they're in wrong order for java)
@@ -73,30 +71,30 @@ public class Main extends Application {
     }
         public void normaliseDatasetValues() {
             //normalise the byte values from the top dataset and add to a new dataset
-            datasetGreyValueTop = new float[IMAGE_DEPTH][IMAGE_HEIGHT][IMAGE_WIDTH];
-            for (int z = 0; z < IMAGE_DEPTH; z++) {
-                for (int y = 0; y < IMAGE_HEIGHT; y++) {
-                    for (int x = 0; x < IMAGE_WIDTH; x++) {
+            datasetGreyValueTop = new float[DATA_SIZE][DATA_SIZE][DATA_SIZE];
+            for (int z = 0; z < DATA_SIZE; z++) {
+                for (int y = 0; y < DATA_SIZE; y++) {
+                    for (int x = 0; x < DATA_SIZE; x++) {
                         datasetGreyValueTop[z][y][x] =  ((float) datasetVoxelByteValuesTop[z][y][x] - (float) min) / ((float) max - (float) min);
                     }
                 }
             }
 
             //used the normalised top dataset to fill the values in the front dataset (z,x,y -> y,z,x)
-            datasetGreyValueFront = new float[IMAGE_HEIGHT][IMAGE_DEPTH][IMAGE_WIDTH];
-            for (int y = 0; y < IMAGE_HEIGHT; y++) {
-                for (int z = 0; z < IMAGE_DEPTH; z++) {
-                    for (int x = 0; x < IMAGE_WIDTH; x++) {
+            datasetGreyValueFront = new float[DATA_SIZE][DATA_SIZE][DATA_SIZE];
+            for (int y = 0; y < DATA_SIZE; y++) {
+                for (int z = 0; z < DATA_SIZE; z++) {
+                    for (int x = 0; x < DATA_SIZE; x++) {
                         datasetGreyValueFront[y][z][x] = datasetGreyValueTop[z][y][x];
                     }
                 }
             }
 
             //use normalised top dataset to fill the values in the side dataset (z,x,y -> x,z,y)
-            datasetGreyValueSide = new float[IMAGE_WIDTH][IMAGE_DEPTH][IMAGE_HEIGHT];
-            for (int x = 0; x < IMAGE_WIDTH; x++) {
-                for (int z = 0; z < IMAGE_DEPTH; z++) {
-                    for (int y = 0; y < IMAGE_HEIGHT; y++) {
+            datasetGreyValueSide = new float[DATA_SIZE][DATA_SIZE][DATA_SIZE];
+            for (int x = 0; x < DATA_SIZE; x++) {
+                for (int z = 0; z < DATA_SIZE; z++) {
+                    for (int y = 0; y < DATA_SIZE; y++) {
                         datasetGreyValueSide[x][z][y] = datasetGreyValueTop[z][y][x];
                     }
                 }
@@ -104,23 +102,23 @@ public class Main extends Application {
         }
 
     public void generateMaximumIntensityProjectionDatasets() {
-        maximumIntensityProjectionImageTop = new float[256][256];
-        for (int y = 0; y < IMAGE_HEIGHT; y++) {
-            for (int x = 0; x < IMAGE_WIDTH; x++) {
+        maximumIntensityProjectionImageTop = new float[DATA_SIZE][DATA_SIZE];
+        for (int y = 0; y < DATA_SIZE; y++) {
+            for (int x = 0; x < DATA_SIZE; x++) {
                 maximumIntensityProjectionImageTop[y][x] = returnMaxFromRay(datasetGreyValueTop, x, y);
             }
         }
 
-        maximumIntensityProjectionImageFront = new float[256][256];
-        for (int y = 0; y < IMAGE_HEIGHT; y++) {
-            for (int x = 0; x < IMAGE_WIDTH; x++) {
+        maximumIntensityProjectionImageFront = new float[DATA_SIZE][DATA_SIZE];
+        for (int y = 0; y < DATA_SIZE; y++) {
+            for (int x = 0; x < DATA_SIZE; x++) {
                 maximumIntensityProjectionImageFront[y][x] = returnMaxFromRay(datasetGreyValueFront, x, y);
             }
         }
 
-        maximumIntensityProjectionImageSide = new float[256][256];
-        for (int y = 0; y < IMAGE_HEIGHT; y++) {
-            for (int x = 0; x < IMAGE_WIDTH; x++) {
+        maximumIntensityProjectionImageSide = new float[DATA_SIZE][DATA_SIZE];
+        for (int y = 0; y < DATA_SIZE; y++) {
+            for (int x = 0; x < DATA_SIZE; x++) {
                 maximumIntensityProjectionImageSide[y][x] = returnMaxFromRay(datasetGreyValueSide, x, y);
             }
         }
@@ -128,7 +126,7 @@ public class Main extends Application {
         public float returnMaxFromRay(float[][][] dataset, int x, int y) {
 
             float currentMaximum = min;
-            for (int k = 0; k < IMAGE_DEPTH; k++) {
+            for (int k = 0; k < DATA_SIZE; k++) {
                 if (dataset[k][y][x] > currentMaximum) {
                     currentMaximum = dataset[k][y][x];
                 }
@@ -139,7 +137,7 @@ public class Main extends Application {
 
     /**Write a new image for the sliced view of the CT scan head (the side being dependent on what string you pass in)*/
     public WritableImage writeNewImage(String imageToWrite){
-        WritableImage newImage = new WritableImage(IMAGE_WIDTH, IMAGE_HEIGHT);
+        WritableImage newImage = new WritableImage(DATA_SIZE, DATA_SIZE);
         PixelWriter newWriter = newImage.getPixelWriter();
 
         float[][][] dataset;
@@ -159,19 +157,19 @@ public class Main extends Application {
                 slice = currentSideSlice;
                 break;
             case "TopMIP": //temporarily convert to 3D array to match datatype requirements, (slice must be 0)
-                float[][][] temp3DimensionalDataTop = new float[1][256][256];
+                float[][][] temp3DimensionalDataTop = new float[1][DATA_SIZE][DATA_SIZE];
                 temp3DimensionalDataTop[0] = maximumIntensityProjectionImageTop;
                 dataset = temp3DimensionalDataTop;
                 slice = 0;
                 break;
             case "FrontMIP":
-                float[][][] temp3DimensionalDataFront = new float[1][256][256];
+                float[][][] temp3DimensionalDataFront = new float[1][DATA_SIZE][DATA_SIZE];
                 temp3DimensionalDataFront[0] = maximumIntensityProjectionImageFront;
                 dataset = temp3DimensionalDataFront;
                 slice = 0;
                 break;
             case "SideMIP":
-                float[][][] temp3DimensionalDataSide = new float[1][256][256];
+                float[][][] temp3DimensionalDataSide = new float[1][DATA_SIZE][DATA_SIZE];
                 temp3DimensionalDataSide[0] = maximumIntensityProjectionImageSide;
                 dataset = temp3DimensionalDataSide;
                 slice = 0;
@@ -183,8 +181,8 @@ public class Main extends Application {
         }
 
         /*Write pixel by pixel the normalised values of the CT scan*/
-        for (int y = 0; y < IMAGE_HEIGHT; y++) {
-            for (int x = 0; x < IMAGE_WIDTH; x++) {
+        for (int y = 0; y < DATA_SIZE; y++) {
+            for (int x = 0; x < DATA_SIZE; x++) {
                 float value = dataset[slice][y][x];
                 Color color = Color.color(value, value, value); //this value needs to be between 0-1 as that's how colors are assigned in java
                 newWriter.setColor(x, y, color);
@@ -280,7 +278,7 @@ public class Main extends Application {
         sideVboxMIP.getChildren().add(sideMIP);
         grid.add(sideVboxMIP, 3, 2);
 
-        Scene scene = new Scene(grid, IMAGE_WIDTH * 4, IMAGE_HEIGHT * 4); //scene(currentpane, xpixelsWide, ypixelsWide)
+        Scene scene = new Scene(grid, DATA_SIZE * 4, DATA_SIZE * 4); //scene(currentpane, xpixelsWide, ypixelsWide)
         stage.setScene(scene); //set that scene onto the stage
         stage.show(); //show the stage
     }
